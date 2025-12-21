cdca4e3bb042b708255f68103cdb078915fe74c8
Ronald Ferreira <ronaldferreiraig@hotmail.com>
2025-12-21 11:17:48 -0300

    feat: create update, findById, save, update, delete to userService and userRepository

--- src/main/java/ronoyaro/study/repository/UserRepository.java
+++ src/main/java/ronoyaro/study/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import ronoyaro.study.domain.User;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,6 +21,27 @@ public class UserRepository {
                 .stream()
                 .filter(user -> user.getFirstName().equalsIgnoreCase(name))
                 .toList();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userData.getUsers()
+                .stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+    }
+
+    public User save(User user) {
+        userData.getUsers().add(user);
+        return user;
+    }
+
+    public void delete(User user) {
+        userData.getUsers().remove(user);
+    }
+
+    public void update(User user) {
+        delete(user);
+        save(user);
     }
 
 }
--- src/main/java/ronoyaro/study/service/UserService.java
+++ src/main/java/ronoyaro/study/service/UserService.java
@@ -1,7 +1,9 @@
 package ronoyaro.study.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 import ronoyaro.study.domain.User;
 import ronoyaro.study.repository.UserRepository;
 
@@ -14,5 +16,25 @@ public class UserService {
 
     public List<User> findAll(String name) {
         return name == null ? repository.findAll() : repository.findByName(name);
+    }
+
+    public User findByIdOrThrowNotFound(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException
+                        (HttpStatus.NOT_FOUND, "User not found"));
+    }
+
+    public User save(User userToSave) {
+        return repository.save(userToSave);
+    }
+
+    public void update(User user) {
+        var userToUpdate = findByIdOrThrowNotFound(user.getId());
+        repository.update(userToUpdate);
+    }
+
+    public void delete(User user) {
+        var userToDelete = findByIdOrThrowNotFound(user.getId());
+        repository.delete(userToDelete);
     }
 }
