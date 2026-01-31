5750d471416d88395c6edba1fd9aba948c2c125c
Ronald Ferreira <ronaldferreiraig@hotmail.com>
2026-01-31 19:17:58 -0300

    feat: implement swagger ui to api documentation

--- src/main/java/ronoyaro/study/controller/UserController.java
+++ src/main/java/ronoyaro/study/controller/UserController.java
@@ -1,15 +1,23 @@
 package ronoyaro.study.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ronoyaro.study.domain.User;
 import ronoyaro.study.dtos.UserPostRequestDTO;
 import ronoyaro.study.dtos.UserPutRequestDTO;
 import ronoyaro.study.dtos.UserResponseDTO;
+import ronoyaro.study.exception.ErrorMessageDefault;
 import ronoyaro.study.mapper.UserMapper;
 import ronoyaro.study.service.UserService;
 
@@ -19,11 +27,19 @@ import java.util.List;
 @RequestMapping("v1/users")
 @RequiredArgsConstructor
 @Slf4j
+@Tag(name = "User API", description = "User related endpoints") //muda o h1 da api no swagger, e a descrição
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
 
-    @GetMapping
+    @GetMapping()//para o swagger identificar o mediatype
+    @Operation(summary = "Get all users", description = "Get all users available in the system",
+            responses = {
+                    @ApiResponse(description = "List all users",
+                            responseCode = "200",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = UserResponseDTO.class))))
+            })
     public ResponseEntity<List<UserResponseDTO>> listAll(@RequestParam(required = false) String firstName) {
 
         log.debug("Request to list all users from name '{}'", firstName);
@@ -37,6 +53,18 @@ public class UserController {
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Find User by id",
+            responses = {
+                    @ApiResponse(description = "Find user by its id",
+                            responseCode = "200",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = UserResponseDTO.class))),
+
+                    @ApiResponse(description = "User not found",
+                            responseCode = "404",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorMessageDefault.class)))
+            })
     public ResponseEntity<UserResponseDTO> findById(@PathVariable Long id) {
 
         log.debug("Request find user by id '{}'", id);
@@ -50,6 +78,7 @@ public class UserController {
     }
 
     @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED) //Swagger
     public ResponseEntity<UserResponseDTO> save(@RequestBody @Valid UserPostRequestDTO userPostRequestDTO) {
 
         log.debug("Request to save user '{}'", userPostRequestDTO.getFirstName());
